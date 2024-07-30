fdb55ff07c48c0392a5a84f23762b280b207c5d8
Карпов Павел Сергеевич <140732486+p7k-S@users.noreply.github.com>
2024-07-31 02:01:15 +0300

    Add files via upload

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,7 +8,6 @@ require("config.mappings")
 require("plugins.lsp")
 require("plugins.cmp")
 require("plugins.snipets")
--- require("plugins.mason")
 
 require("plugins.comments")
 require("plugins.undotree")
